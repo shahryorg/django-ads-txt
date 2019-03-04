16d3f369eda3563c2a335ff990c0311cac718439
Tamer Sherif <tamer.sherif@flyingelephantlab.com>
2019-03-04 12:49:54 +0200

    add rule fields to admin list view

--- ads_txt/admin.py
+++ ads_txt/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Rule
 
-admin.site.register(Rule)
+
+@admin.register(Rule)
+class RuleAdmin(admin.ModelAdmin):
+    list_display = ['domain', 'account_id', 'account_type', 'authority_id']
