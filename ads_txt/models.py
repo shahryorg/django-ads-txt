1fb8af59d0188e1b315a4b2dbe88ea92f5d39237
Tamer Sherif <tamer.sherif@flyingelephantlab.com>
2019-03-04 12:51:36 +0200

    invalidate cached pages on save

--- ads_txt/models.py
+++ ads_txt/models.py
@@ -1,4 +1,5 @@
 from django.contrib.sites.models import Site
+from django.core.cache import cache
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
@@ -17,7 +18,7 @@ class Rule(models.Model):
 
     domain = models.CharField(_('Domain'), max_length=100)
     account_id = models.CharField(_('account ID'), max_length=255)
-    account_type = models.CharField(_('Account Type'), max_length=100, choices=ACCOUNT_TYPE_CHOICES,)
+    account_type = models.CharField(_('Account Type'), max_length=100, choices=ACCOUNT_TYPE_CHOICES)
     authority_id = models.CharField(_('Authority ID'), max_length=255, blank=True, null=True)
 
     sites = models.ManyToManyField(Site, verbose_name=_('sites'), related_name='adstxt')
@@ -28,3 +29,9 @@ class Rule(models.Model):
 
     def __str__(self):
         return self.domain
+
+    def save(self, *args, **kwargs):
+        super(Rule, self).save(*args, **kwargs)
+        # invalidate ads.txt cached pages if any
+        for entry in cache.keys('*ads_txt_*'):
+            cache.delete(entry)
