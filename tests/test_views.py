3584a6c606434320ae00c0d5df6af2651fcdcf90
Lucas Vido <Lucas Vido>
2024-05-20 22:39:04 -0300

    More testcases

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,9 +1,10 @@
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.test import Client, TestCase
 from django.contrib.auth.models import User
 from ads_txt.models import Rule
 
 class AdsTxtTest(TestCase):
+
     def setUp(self):
         self.client = Client()
         Rule.objects.create(
@@ -21,6 +22,7 @@ class AdsTxtTest(TestCase):
 
 
 class UploadAdsTxtTest(TestCase):
+
     def setUp(self):
         self.client = Client()
         self.password = 'my@password123' 
@@ -28,19 +30,47 @@ class UploadAdsTxtTest(TestCase):
             'admin', 'admin@test.com', self.password)
 
     def test_bulk_upload_unauthorized(self):
-        response = self.client.get(reverse('ads_txt_bulk_upload'), follow=True)
+        response = self.client.get(
+            reverse_lazy('ads_txt_bulk_upload'), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get('content-type'), 'text/html; charset=utf-8')
-        self.assertIn('Log in | Django site admin', str(response.content))
+        self.assertIn('text/html;', response.get('content-type'))
+        self.assertContains(response, 'Log in | Django site admin')
 
     def test_bulk_upload_get(self):
         self.client.login(username=self.user.username, password=self.password)
-        response = self.client.get(reverse('ads_txt_bulk_upload'))
+        response = self.client.get(
+            reverse_lazy('ads_txt_bulk_upload'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(response.get('content-type'), 'text/html; charset=utf-8')
+        self.assertIn('text/html;', response.get('content-type'))
 
     def test_bulk_upload_post(self):
+
+        data = {'ads_rules': '\n'.join([
+            "test.com, 121243d, DIRECT",
+            "test.com, re121243d, RESELLER, re121243d"])
+        }
+
         self.client.login(username=self.user.username, password=self.password)
-        response = self.client.post(reverse('ads_txt_bulk_upload'))
+        response = self.client.post(
+            reverse_lazy('ads_txt_bulk_upload'), data, follow=True)
+
         self.assertEqual(response.status_code, 200)
-        self.assertIn(response.get('content-type'), 'text/html; charset=utf-8')
+        self.assertIn('text/html;', response.get('content-type'))
+        self.assertContains(response, 'rules updated with success')
+
+    def test_bulk_upload_post_invalid(self):
+
+        data = {'ads_rules': '\n'.join([
+            "test.com121243d, DIRECT",
+            "test.com, re121243d, RESELLER, re121243d\r\n",
+            "\r\n",
+            "test.com, re121243d, re121243d"
+            "\n"])
+        }
+
+        self.client.login(username=self.user.username, password=self.password)
+        response = self.client.post(
+            reverse_lazy('ads_txt_bulk_upload'), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Failed to update')
+        self.assertContains(response, 'Error reading lines')
