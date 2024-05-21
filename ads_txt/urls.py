565dea13beca93c18bfd526d098e9c4be7f0d6f6
Lucas Vido <Lucas Vido>
2024-05-20 22:39:34 -0300

    Bulk Upload View

--- ads_txt/urls.py
+++ ads_txt/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
-from ads_txt.views import rules_list
+from ads_txt.views import rules_list, bulk_upload_rules
 
 urlpatterns = [
+    url(r'upload/$', bulk_upload_rules, name='ads_txt_bulk_upload'),
     url(r'^$', rules_list, name='ads_txt_rule_list'),
 ]
