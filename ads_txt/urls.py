4635f45d7a8f9d2ac4b3a936eca20d0c35d4d3af
Lucas Vido <vidov8@gmail.com>
2025-02-06 12:31:43 -0300

    (urls.W002) Your URL pattern '/upload/$' [name='ads_txt_bulk_upload'] has a route beginning with a '/'.

--- ads_txt/urls.py
+++ ads_txt/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from ads_txt.views import rules_list, bulk_upload_rules
 
 urlpatterns = [
-    url(r'/upload/$', bulk_upload_rules, name='ads_txt_bulk_upload'),
-    url(r'^$', rules_list, name='ads_txt_rule_list'),
+    url(r'^ads_txt/upload$', bulk_upload_rules, name='ads_txt_bulk_upload'),
+    url(r'^ads.txt$', rules_list, name='ads_txt_rule_list'),
 ]
